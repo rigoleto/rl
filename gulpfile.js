fb6374c3d9fbc1a9f83212281af2033392fc27c6
Reid <reid.lynch@gmail.com>
2014-11-09 21:28:45 -0700

    css & js path fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,41 +3,44 @@ var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
 var minifyCss = require('gulp-minify-css')
 var mainBowerFiles = require('main-bower-files')
+var rename = require("gulp-rename")
 
 var paths = {
   js: ['assets/js/*'],
   css: ['assets/css/*'],
   images: 'assets/images/*',
   dist: 'dist'
-};
-
-// console.log(mainBowerFiles({ filter: /\.js/ }))
+}
 
 paths.js = paths.js.concat(mainBowerFiles({ filter: /\.js/ }))
 paths.css = paths.css.concat(mainBowerFiles({ filter: /\.css/ }))
 
 gulp.task('js', function() {
-  gulp.src(paths.js)
+  return gulp.src(paths.js)
     .pipe(concat('all.js'))
-    .pipe(uglify())
     .pipe(gulp.dest(paths.dist))
-});
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(gulp.dest(paths.dist))
+})
 
 gulp.task('css', function() {
-  gulp.src(paths.css)
+  return gulp.src(paths.css)
     .pipe(concat('all.css'))
-    .pipe(minifyCss())
     .pipe(gulp.dest(paths.dist))
-});
+    .pipe(minifyCss())
+    .pipe(rename('all.min.css'))
+    .pipe(gulp.dest(paths.dist))
+})
 
 gulp.task('images', function() {
-  gulp.src(paths.images)
-    .pipe(gulp.dest(paths.dist));
-});
+  return gulp.src(paths.images)
+    .pipe(gulp.dest(paths.dist))
+})
 
 gulp.task('watch', function() {
-  gulp.watch(paths.js, ['js']);
-  gulp.watch(paths.css, ['css']);
-});
+  gulp.watch(paths.js, ['js'])
+  gulp.watch(paths.css, ['css'])
+})
 
-gulp.task('default', ['js', 'css', 'images']);
+gulp.task('default', ['js', 'css', 'images'])
